9e2662370128b9ebe5505cbd939049ce59bffa26
SukiEva <dev.suki@outlook.com>
2022-10-02 11:46:25 +0800

    init

---
+++ src/main/java/com/github/gitfy/gitfyapi/GitfyApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.gitfy.gitfyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitfyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitfyApiApplication.class, args);
+    }
+
+}
